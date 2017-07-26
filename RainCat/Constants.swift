43bf83f25f7f9105dccf0caa0913f3ca4bf21d7d
Homosum <120652394@qq.com>
2017-07-26 14:30:53 +0800

    s

---
+++ RainCat/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  RainCat
+//
+//  Created by Homosum on 17/7/26.
+//  Copyright © 2017年 Thirteen23. All rights reserved.
+//
+
+import Foundation
+let WorldCategory :UInt32 = 0x1 << 1
+let RainDropCategory :UInt32 = 0x1 << 2
+let FloorCategory :UInt32 = 0x1 << 3
+
+
+
+		
